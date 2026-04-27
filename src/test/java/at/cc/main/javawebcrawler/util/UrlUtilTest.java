ce0b347a03a27602005b8d6ae82a0b4244d7fe06
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-28 01:36:17 +0200

    add tests for UrlUtil

--- src/test/java/at/cc/main/javawebcrawler/util/UrlUtilTest.java
+++ src/test/java/at/cc/main/javawebcrawler/util/UrlUtilTest.java
@@ -3,6 +3,7 @@ package at.cc.main.javawebcrawler.util;
 import org.junit.jupiter.api.Test;
 
 import java.net.URISyntaxException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +21,77 @@ public class UrlUtilTest {
         UrlUtil urlUtil = new UrlUtil();
 
         assertNull(urlUtil.getDomain("test"));
+    }
+
+    @Test
+    void allowExactDomainMatch() {
+        String url = "https://aau.at";
+        List<String> allowed = List.of("aau.at");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void allowWwwVariation() {
+        String url = "https://www.aau.at";
+        List<String> allowed = List.of("aau.at");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void allowSubdomain() {
+        String url = "https://test.aau.at";
+        List<String> allowed = List.of("aau.at");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void allowNestedSubdomain() {
+        String url = "https://another.test.aau.at";
+        List<String> allowed = List.of("aau.at");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void rejectSimilarButInvalidDomain() {
+        String url = "https://notaau.at";
+        List<String> allowed = List.of("aau.at");
+
+        assertFalse(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void handleAllowedDomainWithWww() {
+        String url = "https://aau.at";
+        List<String> allowed = List.of("www.aau.at");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void returnFalseForInvalidUrl() {
+        String url = "invalid-url";
+        List<String> allowed = List.of("aau.at");
+
+        assertFalse(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void returnFalseWhenHostIsNull() {
+        String url = "http:///invalid";
+        List<String> allowed = List.of("aau.at");
+
+        assertFalse(UrlUtil.isAllowedDomain(url, allowed));
+    }
+
+    @Test
+    void workWithMultipleAllowedDomains() {
+        String url = "https://test.com";
+        List<String> allowed = List.of("aau.at", "test.com");
+
+        assertTrue(UrlUtil.isAllowedDomain(url, allowed));
     }
 }
