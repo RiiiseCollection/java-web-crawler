a0f37c529527a78b955984647cf7789784ea649a
Maverick Haas <191795490+mav42@users.noreply.github.com>
2026-04-28 01:57:37 +0200

    implement crawler engine logic

--- src/main/java/at/cc/main/javawebcrawler/Main.java
+++ src/main/java/at/cc/main/javawebcrawler/Main.java
@@ -1,7 +1,12 @@
 package at.cc.main.javawebcrawler;
 
+import at.cc.main.javawebcrawler.engine.CrawlerEngine;
 import at.cc.main.javawebcrawler.exception.InputValidationException;
+import at.cc.main.javawebcrawler.report.MarkdownReportGenerator;
 import at.cc.main.javawebcrawler.util.InputValidator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -9,6 +14,28 @@ public class Main {
 
         try {
             InputValidator.validateInput(args);
+
+            String startUrl = args[0];
+            int maxDepth = Integer.parseInt(args[1]);
+            List<String> allowedDomains = new ArrayList<>();
+
+            for (int i = 2; i < args.length; i++) {
+                allowedDomains.add(args[i]);
+            }
+
+            System.out.println("Start URL: " + startUrl);
+            System.out.println("Max depth: " + maxDepth);
+            System.out.println("Allowed domains: " + allowedDomains);
+
+            CrawlerEngine crawler = new CrawlerEngine(maxDepth, allowedDomains);
+            crawler.crawl(startUrl);
+
+            MarkdownReportGenerator reportGenerator = new MarkdownReportGenerator();
+            reportGenerator.generateReport(crawler.getCrawledPages());
+
+            System.out.println("\nCrawling completed successfully!");
+            System.out.println("Pages crawled: " + crawler.getVisitedUrls().size());
+
         } catch (InputValidationException e) {
             System.err.println(e.getMessage());
             System.exit(2);
