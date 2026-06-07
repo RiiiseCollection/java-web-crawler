fede41c338266b734dab7d39718497d55f300845
Johannes <75024594+Riiise1406@users.noreply.github.com>
2026-06-07 16:18:28 +0200

    removed null return in UrlUtil

--- src/main/java/at/cc/main/javawebcrawler/util/UrlUtil.java
+++ src/main/java/at/cc/main/javawebcrawler/util/UrlUtil.java
@@ -3,23 +3,25 @@ package at.cc.main.javawebcrawler.util;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 public class UrlUtil {
 
-    public static String getDomain(String url) {
+    public static Optional<String> getDomain(String url) {
         try {
             URI uri = new URI(url);
-            return uri.getHost();
-        } catch (URISyntaxException e) {
-            return null;
+            return Optional.of(uri.getHost());
+        } catch (URISyntaxException | NullPointerException e) {
+            return Optional.empty();
         }
     }
 
     public static boolean isAllowedDomain(String url, List<String> allowedDomains) {
-        String host = getDomain(url);
-        if (host == null) {
-            return false;
-        }
+        Optional<String> hostOptional = getDomain(url);
+
+        if(hostOptional.isEmpty()) return false;
+
+        String host = hostOptional.get();
 
         String cleanHost = host.startsWith("www.") ? host.substring(4) : host;
 
